c579216e5d5aaec14a01edd43a5489231881ffef
GeorgeRankin <Wyvern1200>
2017-06-24 01:33:43 +0100

    Basic FOV detection and chasing enemy

---
+++ Assets/Scripts/Chase_2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase_2 : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 direction;
+	private bool Overlap;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Camera (eye)");
+	}
+
+	void OnTriggerEnter (Collider col){
+		if (col.tag == "FOV")
+			Overlap = true;
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.tag == "FOV") {
+			Overlap = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Overlap == false) {
+			direction = player.transform.position - this.transform.position;
+			//direction.y = 0;
+
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
+			this.transform.position = Vector3.MoveTowards (transform.position, player.transform.position, Time.deltaTime / 0.5f);
+		}
+	}
+
+}
---
+++ Assets/Scripts/FOV_Detection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOV_Detection : MonoBehaviour {
+
+	public GameObject InvisibleMesh;
+	public GameObject TriggerMesh;
+
+	/*
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Prop") {
+			col.gameObject.GetComponent<Renderer> ().enabled = false;
+			Debug.Log ("hit");
+		}
+		//col.gameObject.SetActive(false);
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.tag == "Prop") {
+			col.gameObject.GetComponent<Renderer> ().enabled = true;
+			Debug.Log ("leave");
+		}
+
+	}
+*/
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject == TriggerMesh) {
+			InvisibleMesh.GetComponent<Renderer> ().enabled = true;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
